bfec491d3eb1a52f785a8e66f4202f63ff5b18bd
LaptopHP-ian <ianagung1986@gmail.com>
2021-07-26 14:55:21 +0700

    Update IP Controller

--- Apptesis-03-loadBalancer13-02.py
+++ Apptesis-03-loadBalancer13-02.py
@@ -45,9 +45,9 @@ class loadBalancer13(app_manager.RyuApp):
 
 ############Assigning IP address to TCP servers (H1, H2, and H3)
         self.serverIP1="192.168.147.6"
-        self.serverMac1="00:00:00:00:00:01"
-        self.serverIP2="10.0.0.2"
-        self.serverMac2="00:00:00:00:00:02"
+        self.serverMac1="08:00:27:4E:58:A6"
+        self.serverIP2="192.168.147.5"
+        self.serverMac2="08:00:27:55:6b:96"
         # self.serverIP1="10.0.0.1"
         # self.serverMac1="00:00:00:00:00:01"
         # self.serverIP2="10.0.0.2"
@@ -92,11 +92,11 @@ class loadBalancer13(app_manager.RyuApp):
                                     match=match, instructions=inst)
         datapath.send_msg(mod)
 
-    ############Generate ARP reply packet for ARP request to controller (IP: 10.0.0.100).
+    ############Generate ARP reply packet for ARP request to controller (IP: 192.168.147.100).
     #srcMac can be any value to set controller MAC address for controller IP address.
     def arpReplyGenerate(self, dstMac, dstIp):
         srcMac = "11:22:33:ab:cd:ef"
-        srcIp = "10.0.0.100"
+        srcIp = "192.168.147.100"
 
         packetReply = packet.Packet()
         etherReply = ethernet.ethernet(dstMac, srcMac,0x0806)
@@ -132,14 +132,14 @@ class loadBalancer13(app_manager.RyuApp):
         self.logger.info("\n Packet-In - DPID: %s SMAC: %s DMAC: %s InPort: %s", dpid, src, dst, in_port)
 
 ############ARP Reply handling
-#Send ARP reply for ARP request to controller (IP: 10.0.0.100)
+#Send ARP reply for ARP request to controller (IP: 192.168.147.100)
 
         if(eth.ethertype==0x0806):
-
+            #ethertype==0x0806==ARP
             self.logger.info("\n Reached inside of first ARP type check-------->")
             arpContents=pkt.get_protocols(arp.arp)[0]
-            if((arpContents.dst_ip=="10.0.0.100") and (arpContents.opcode==1)):
-                self.logger.info("\n Reached inside of ARP reply for 10.0.0.100-------->")
+            if((arpContents.dst_ip=="192.168.147.100") and (arpContents.opcode==1)):
+                self.logger.info("\n Reached inside of ARP reply for 192.168.147.100-------->")
                 packetReply=self.arpReplyGenerate(arpContents.src_mac,arpContents.src_ip)
                 actionsServer=[parser.OFPActionOutput(in_port)]
                 arpServer=parser.OFPPacketOut(datapath=datapath,in_port=ofproto.OFPP_ANY,data=packetReply.data,actions=actionsServer,buffer_id=0xffffffff)
@@ -155,8 +155,8 @@ class loadBalancer13(app_manager.RyuApp):
 #S1 or H1 is used as server for count of 1
 #S2 or H2 is used as server for count of 2
 #S3 or H3 is used as server for count of 3
-
-        self.logger.info("\n Reached inside of first TCP type check-------->")
+        self.logger.info("\n Reached inside NOT AN ARP type check-------->")
+       
 
         if(self.serverCount==1):
             serverIP=self.serverIP1
@@ -169,20 +169,22 @@ class loadBalancer13(app_manager.RyuApp):
             serverMac=self.serverMac3        
 
         if(eth.ethertype==0x0800):
+            self.logger.info("\n Reached inside of first IP type check-------->")
             ipContents=pkt.get_protocols(ipv4.ipv4)[0]
-            if((ipContents.dst=="10.0.0.100") and (ipContents.proto==0x06)):
+            if((ipContents.dst=="192.168.147.100") and (ipContents.proto==0x06)):
+                self.logger.info("\n Reached inside of  TCP type && IP controller check-------->")
                 tcpContents=pkt.get_protocols(tcp.tcp)[0]
 
                 #Perform TCP action only if matching TCP properties
                 match1=parser.OFPMatch(in_port=in_port,eth_type=eth.ethertype,eth_src=eth.src,eth_dst=eth.dst,ip_proto=ipContents.proto,ipv4_src=ipContents.src,ipv4_dst=ipContents.dst,tcp_src=tcpContents.src_port,tcp_dst=tcpContents.dst_port)
 
                 #Send host TCP segments to destination server using destination server port connected to controller
-                actions1=[parser.OFPActionSetField(ipv4_src="10.0.0.100"),parser.OFPActionSetField(eth_dst=serverMac),parser.OFPActionSetField(ipv4_dst=serverIP),parser.OFPActionOutput(self.serverCount)]
+                actions1=[parser.OFPActionSetField(ipv4_src="192.168.147.100"),parser.OFPActionSetField(eth_dst=serverMac),parser.OFPActionSetField(ipv4_dst=serverIP),parser.OFPActionOutput(self.serverCount)]
 
                 ipInst1=[parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,actions1)] 
                 cookie1=random.randint(0, 0xffffffffffffffff)
 
-                #Create flow for incoming TCP segments from host to server through controller (IP: 10.0.0.100)
+                #Create flow for incoming TCP segments from host to server through controller (IP: 192.168.147.100)
                 flowMod1=parser.OFPFlowMod(datapath=datapath,match=match1,idle_timeout=7,instructions=ipInst1,buffer_id=msg.buffer_id,cookie=cookie1)
 
                 self.logger.info("\n Added flow for Host to Server condition------->")
@@ -190,7 +192,7 @@ class loadBalancer13(app_manager.RyuApp):
 
                 self.logger.info("\n Client-LB - SIP: "+str(ipContents.src)+" DIP: "+str(ipContents.dst))
 
-                self.logger.info("\n LB-Server - SIP: 10.0.0.100 DIP: "+str(serverIP))
+                self.logger.info("\n LB-Server - SIP: 192.168.147.100 DIP: "+str(serverIP))
 
                 #Add flow in the flow table of the virtual switch
 
@@ -199,21 +201,21 @@ class loadBalancer13(app_manager.RyuApp):
                 ############TCP Server to Host
 
                 #Perform TCP action only if matching TCP properties
-                match2=parser.OFPMatch(self.serverCount,eth_type=eth.ethertype,eth_src=serverMac,eth_dst="11:22:33:ab:cd:ef",ip_proto=ipContents.proto,ipv4_src=serverIP,ipv4_dst="10.0.0.100",tcp_src=tcpContents.dst_port,tcp_dst=tcpContents.src_port)
+                match2=parser.OFPMatch(self.serverCount,eth_type=eth.ethertype,eth_src=serverMac,eth_dst="11:22:33:ab:cd:ef",ip_proto=ipContents.proto,ipv4_src=serverIP,ipv4_dst="192.168.147.100",tcp_src=tcpContents.dst_port,tcp_dst=tcpContents.src_port)
 
                 #Send server TCP segments to host using source host port connected to controller
-                actions2=[parser.OFPActionSetField(eth_src="11:22:33:ab:cd:ef"),parser.OFPActionSetField(ipv4_src="10.0.0.100"),parser.OFPActionSetField(eth_dst=eth.src),parser.OFPActionSetField(ipv4_dst=ipContents.src),parser.OFPActionOutput(in_port)]
+                actions2=[parser.OFPActionSetField(eth_src="11:22:33:ab:cd:ef"),parser.OFPActionSetField(ipv4_src="192.168.147.100"),parser.OFPActionSetField(eth_dst=eth.src),parser.OFPActionSetField(ipv4_dst=ipContents.src),parser.OFPActionOutput(in_port)]
 
                 ipInst2=[parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,actions2)] 
 
                 cookie2=random.randint(0, 0xffffffffffffffff)
 
-                #Create flow for TCP segments from server to host through controller (IP: 10.0.0.100)
+                #Create flow for TCP segments from server to host through controller (IP: 192.168.147.100)
                 flowMod2=parser.OFPFlowMod(datapath=datapath,match=match2,idle_timeout=7,instructions=ipInst2,cookie=cookie2)
 
-                self.logger.info("\n Server-LB - SIP: "+str(serverIP)+" DIP: 10.0.0.100")
+                self.logger.info("\n Server-LB - SIP: "+str(serverIP)+" DIP: 192.168.147.100")
 
-                self.logger.info("\n LB-Client - SIP: 10.0.0.100 DIP: "+str(ipContents.src))
+                self.logger.info("\n LB-Client - SIP: 192.168.147.100 DIP: "+str(ipContents.src))
 
                 #Add flow in the flow table of the virtual switch
 
