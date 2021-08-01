6e6ae21bfa3a37f599d6d6f9dc2890096346047a
ianagung <ian.agung@len.co.id>
2021-08-01 21:30:34 +0700

    Update AppFuzyTest.py

--- AppFuzyTest.py
+++ AppFuzyTest.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python3
+import paho.mqtt.client as mqtt
+import numpy as np
+import skfuzzy as fuzz
+import multitimer
+import time
+from matplotlib import pyplot as plt
 
 sys.path.append(".")
 from fuzzyMam3inCMT import Fuzzy
 
-#do fuzzy untuk setiap server  
+#do fuzzy untuk setiap server
+cpu_val = 50
+mem_val = 50
+truput_val = 50 
 myFuzzy = Fuzzy(cpu_val, mem_val, truput_val)
 delta_ld_window = myFuzzy.get_fuzzy()
 
