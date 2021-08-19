72a2088a76e605ea1a88d85ebee9ef9593cf5693
ianagung <ian.agung@len.co.id>
2021-08-19 17:35:59 +0700

    update uji

--- AppUji.sh
+++ AppUji.sh
@@ -3,12 +3,12 @@
 Kalimat="Pengujian Load Balancing dimulai - Uji Tanpa Beban Algoritma Random"
 echo $Kalimat |& tee LoadTest01-RR01.txt
 # for loop
-for i in {0..1..1}
+for i in {0..19..1}
   do 
     # Load python toggle start
     python3 AppToggleUjiStart.py
     # Run siege
-    siege http://192.168.146.10:85 -c 2 -t 1m |& tee -a LoadTest01-RR01.txt
+    siege http://192.168.146.10:85 -c 250 -t 1m |& tee -a LoadTest01-RR01.txt
     #output=$(siege http://www.len.co.id -c 2 -t 1m)
     # Output siege append to file
     #$output >> LoadTest01.txt
