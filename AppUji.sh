2e198dfbb2fac5aa7ff08098a6ddf005965e44b4
ianagung <ian.agung@len.co.id>
2021-08-20 21:22:50 +0700

    Uji1 RR01

--- AppUji.sh
+++ AppUji.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #defining a variable
 Kalimat="Pengujian Load Balancing dimulai - Uji Tanpa Beban Algoritma Fuzzy"
-echo $Kalimat |& tee LoadTest01-Fuzzy01.txt
+echo $Kalimat |& tee Uji01-RR01.txt
 # for loop
-for i in {0..4..1}
+for i in {0..29..1}
   do 
     # Load python toggle start
     python3 AppToggleUjiStart.py
     # Run siege
-    ab -n 100 -c 2 http://192.168.146.10:1000/ |& tee -a LoadTest01-Fuzzy01.txt
+    ab -n 100 -c 5 http://192.168.146.10:1000/ |& tee -a Uji01-RR01.txt
     #siege http://192.168.146.10:85 -c 250 -t 1m |& tee -a LoadTest01-RR01.txt
     #output=$(siege http://www.len.co.id -c 2 -t 1m)
     # Output siege append to file
@@ -17,7 +17,7 @@ for i in {0..4..1}
     echo "selesai"
     # Load python toggle stop
     python3 AppToggleUjiStop.py
-    sleep 5    
+    sleep 1    
  done
 # End loop
 
