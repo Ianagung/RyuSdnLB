669645ce1ecbd9c548c38d20683dee45db5d9241
ianagung <ian.agung@len.co.id>
2021-08-22 05:01:29 +0700

    UJi02 RR01

--- AppUji.sh
+++ AppUji.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #defining a variable
-Kalimat="Pengujian Load Balancing dimulai - Uji Beban Algoritma Random"
-echo $Kalimat |& tee Uji02-Rd01.txt
+Kalimat="Pengujian Load Balancing dimulai - Uji Beban Algoritma Round Robin"
+echo $Kalimat |& tee Uji02-RR01.txt
 # for loop
 for i in {0..29..1}
   do 
     # Load python toggle start
     python3 AppToggleUjiStart.py
     # Run siege
-    ab -n 100 -c 5 http://192.168.146.100:1000/ |& tee -a Uji02-Rd01.txt
+    ab -n 100 -c 5 http://192.168.146.100:1000/ |& tee -a Uji02-RR01.txt
     #siege http://192.168.146.10:85 -c 250 -t 1m |& tee -a LoadTest01-RR01.txt
     #output=$(siege http://www.len.co.id -c 2 -t 1m)
     # Output siege append to file
