fccd33c71462b654b341ffb21be6d905dbb124f3
ianagung <ian.agung@len.co.id>
2021-08-22 22:34:09 +0700

    Uji03 Random

--- AppUji.sh
+++ AppUji.sh
@@ -13,7 +13,7 @@ for i in {0..29..1}
     python3 AppToggleUjiStart.py
     echo "Tes request 100" |& tee -a Uji03-Rd01.txt
     # Run ab
-    ab -n 100 -c 10 http://192.168.146.100:1000/ |& tee -a Uji03-Rd01.txt
+    ab -n 100 -c 5 http://192.168.146.100:1000/ |& tee -a Uji03-Rd01.txt
     # Load python toggle stop
     python3 AppToggleUjiStop.py
     sleep 1
@@ -35,7 +35,7 @@ for i in {0..29..1}
     python3 AppToggleUjiStart.py
     echo "Tes request 200" |& tee -a Uji03-Rd02.txt
     # Run ab
-    ab -n 200 -c 10 http://192.168.146.100:1000/ |& tee -a Uji03-Rd02.txt
+    ab -n 200 -c 5 http://192.168.146.100:1000/ |& tee -a Uji03-Rd02.txt
     # Load python toggle stop
     python3 AppToggleUjiStop.py
     sleep 1
@@ -57,7 +57,7 @@ for i in {0..29..1}
     python3 AppToggleUjiStart.py
     echo "Tes request 300" |& tee -a Uji03-Rd03.txt
     # Run ab
-    ab -n 300 -c 10 http://192.168.146.100:1000/ |& tee -a Uji03-Rd03.txt
+    ab -n 300 -c 5 http://192.168.146.100:1000/ |& tee -a Uji03-Rd03.txt
     # Load python toggle stop
     python3 AppToggleUjiStop.py
     sleep 1
@@ -79,7 +79,7 @@ for i in {0..29..1}
     python3 AppToggleUjiStart.py
     echo "Tes request 400" |& tee -a Uji03-Rd04.txt
     # Run ab
-    ab -n 400 -c 10 http://192.168.146.100:1000/ |& tee -a Uji03-Rd04.txt
+    ab -n 400 -c 5 http://192.168.146.100:1000/ |& tee -a Uji03-Rd04.txt
     # Load python toggle stop
     python3 AppToggleUjiStop.py
     sleep 1
@@ -100,7 +100,7 @@ for i in {0..29..1}
     python3 AppToggleUjiStart.py
     echo "Tes request 500" |& tee -a Uji03-Rd05.txt
     # Run ab
-    ab -n 500 -c 10 http://192.168.146.100:1000/ |& tee -a Uji03-Rd05.txt
+    ab -n 500 -c 5 http://192.168.146.100:1000/ |& tee -a Uji03-Rd05.txt
     # Load python toggle stop
     python3 AppToggleUjiStop.py
     sleep 1
