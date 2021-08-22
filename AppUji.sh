4be6d97e907b5ba2ff3e53b115cff5e1a1045a5a
ianagung <ian.agung@len.co.id>
2021-08-22 22:08:20 +0700

    Uji03 random

--- AppUji.sh
+++ AppUji.sh
@@ -33,9 +33,9 @@ for i in {0..29..1}
 
     # Load python toggle start
     python3 AppToggleUjiStart.py
-    echo "Tes request 1000" |& tee -a Uji03-Rd02.txt
+    echo "Tes request 200" |& tee -a Uji03-Rd02.txt
     # Run ab
-    ab -n 1000 -c 10 http://192.168.146.100:1000/ |& tee -a Uji03-Rd02.txt
+    ab -n 200 -c 10 http://192.168.146.100:1000/ |& tee -a Uji03-Rd02.txt
     # Load python toggle stop
     python3 AppToggleUjiStop.py
     sleep 1
@@ -55,9 +55,9 @@ for i in {0..29..1}
 
    # Load python toggle start
     python3 AppToggleUjiStart.py
-    echo "Tes request 2000" |& tee -a Uji03-Rd03.txt
+    echo "Tes request 300" |& tee -a Uji03-Rd03.txt
     # Run ab
-    ab -n 2000 -c 10 http://192.168.146.100:1000/ |& tee -a Uji03-Rd03.txt
+    ab -n 300 -c 10 http://192.168.146.100:1000/ |& tee -a Uji03-Rd03.txt
     # Load python toggle stop
     python3 AppToggleUjiStop.py
     sleep 1
@@ -77,9 +77,9 @@ for i in {0..29..1}
 
     # Load python toggle start
     python3 AppToggleUjiStart.py
-    echo "Tes request 3000" |& tee -a Uji03-Rd04.txt
+    echo "Tes request 400" |& tee -a Uji03-Rd04.txt
     # Run ab
-    ab -n 3000 -c 10 http://192.168.146.100:1000/ |& tee -a Uji03-Rd04.txt
+    ab -n 400 -c 10 http://192.168.146.100:1000/ |& tee -a Uji03-Rd04.txt
     # Load python toggle stop
     python3 AppToggleUjiStop.py
     sleep 1
@@ -98,9 +98,9 @@ for i in {0..29..1}
     echo "Testing number $i " |& tee -a Uji03-Rd05.txt
     # Load python toggle start
     python3 AppToggleUjiStart.py
-    echo "Tes request 4000" |& tee -a Uji03-Rd05.txt
+    echo "Tes request 500" |& tee -a Uji03-Rd05.txt
     # Run ab
-    ab -n 4000 -c 10 http://192.168.146.100:1000/ |& tee -a Uji03-Rd05.txt
+    ab -n 500 -c 10 http://192.168.146.100:1000/ |& tee -a Uji03-Rd05.txt
     # Load python toggle stop
     python3 AppToggleUjiStop.py
     sleep 1
