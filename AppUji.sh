4dad8001d8c5199d7249d08dd20ed2a0c113763c
ianagung <ian.agung@len.co.id>
2021-08-22 08:23:38 +0700

    Uji02 RR

--- AppUji.sh
+++ AppUji.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #defining a variable
 Kalimat="Pengujian Load Balancing dimulai - Uji Beban Algoritma Round Robin"
-echo $Kalimat |& tee Uji02-Rd01.txt
+echo $Kalimat |& tee Uji02-RR01.txt
 # for loop
 for i in {0..29..1}
   do 
@@ -10,7 +10,7 @@ for i in {0..29..1}
     #sleep 5
     #echo "kosongan" |& tee Uji02-0.txt
     # Run siege
-    ab -n 100 -c 5 http://192.168.146.100:1000/ |& tee -a Uji02-Rd01.txt
+    ab -n 100 -c 5 http://192.168.146.100:1000/ |& tee -a Uji02-RR01.txt
     #siege http://192.168.146.10:85 -c 250 -t 1m |& tee -a LoadTest01-RR01.txt
     #output=$(siege http://www.len.co.id -c 2 -t 1m)
     # Output siege append to file
