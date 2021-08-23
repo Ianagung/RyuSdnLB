4c414bb53468f7cfbc95928c69e7bd2b6e47e4ca
ianagung <ian.agung@len.co.id>
2021-08-23 14:18:16 +0700

    Uji04 MIn RT

--- AppUji.sh
+++ AppUji.sh
@@ -1,111 +1,111 @@
 #!/bin/bash
 #defining a variable
-Kalimat="Pengujian Load Balancing dimulai - Uji4 Dengan Beban Random"
-echo $Kalimat |& tee Uji04-Rd01.txt
+Kalimat="Pengujian Load Balancing dimulai - Uji4 Dengan Min Respon Time"
+echo $Kalimat |& tee Uji04-RT01.txt
 # for loop
 for i in {0..29..1}
   do 
     
     #sleep 5
-    echo "Testing number $i " |& tee -a Uji04-Rd01.txt
+    echo "Testing number $i " |& tee -a Uji04-RT01.txt
 
     # Load python toggle start
     python3 AppToggleUjiStart.py
-    echo "Tes request 100" |& tee -a Uji04-Rd01.txt
+    echo "Tes request 100" |& tee -a Uji04-RT01.txt
     # Run ab
-    ab -n 100 -c 10 http://192.168.146.100:1000/ |& tee -a Uji04-Rd01.txt
+    ab -n 100 -c 10 http://192.168.146.100:1000/ |& tee -a Uji04-RT01.txt
     # Load python toggle stop
     python3 AppToggleUjiStop.py
     sleep 1
 
-    echo "selesai" |& tee -a Uji04-Rd01.txt
+    echo "selesai" |& tee -a Uji04-RT01.txt
     sleep 1    
  done
 # End loop
 
-echo $Kalimat |& tee Uji04-Rd02.txt
+echo $Kalimat |& tee Uji04-RT02.txt
 # for loop
 for i in {0..29..1}
   do 
     
     #sleep 5
-    echo "Testing number $i " |& tee -a Uji04-Rd02.txt
+    echo "Testing number $i " |& tee -a Uji04-RT02.txt
 
     # Load python toggle start
     python3 AppToggleUjiStart.py
-    echo "Tes request 200" |& tee -a Uji04-Rd02.txt
+    echo "Tes request 200" |& tee -a Uji04-RT02.txt
     # Run ab
-    ab -n 200 -c 10 http://192.168.146.100:1000/ |& tee -a Uji04-Rd02.txt
+    ab -n 200 -c 10 http://192.168.146.100:1000/ |& tee -a Uji04-RT02.txt
     # Load python toggle stop
     python3 AppToggleUjiStop.py
     sleep 1
 
-    echo "selesai" |& tee -a Uji04-Rd02.txt
+    echo "selesai" |& tee -a Uji04-RT02.txt
     sleep 1    
  done
 # End loop
 
-echo $Kalimat |& tee Uji04-Rd03.txt
+echo $Kalimat |& tee Uji04-RT03.txt
 # for loop
 for i in {0..29..1}
   do 
     
     #sleep 5
-    echo "Testing number $i " |& tee -a Uji04-Rd03.txt
+    echo "Testing number $i " |& tee -a Uji04-RT03.txt
 
    # Load python toggle start
     python3 AppToggleUjiStart.py
-    echo "Tes request 300" |& tee -a Uji04-Rd03.txt
+    echo "Tes request 300" |& tee -a Uji04-RT03.txt
     # Run ab
-    ab -n 300 -c 10 http://192.168.146.100:1000/ |& tee -a Uji04-Rd03.txt
+    ab -n 300 -c 10 http://192.168.146.100:1000/ |& tee -a Uji04-RT03.txt
     # Load python toggle stop
     python3 AppToggleUjiStop.py
     sleep 1
 
-    echo "selesai" |& tee -a Uji04-Rd03.txt
+    echo "selesai" |& tee -a Uji04-RT03.txt
     sleep 1    
  done
 # End loop
 
-echo $Kalimat |& tee Uji04-Rd04.txt
+echo $Kalimat |& tee Uji04-RT04.txt
 # for loop
 for i in {0..29..1}
   do 
     
     #sleep 5
-    echo "Testing number $i " |& tee -a Uji04-Rd04.txt
+    echo "Testing number $i " |& tee -a Uji04-RT04.txt
 
     # Load python toggle start
     python3 AppToggleUjiStart.py
-    echo "Tes request 400" |& tee -a Uji04-Rd04.txt
+    echo "Tes request 400" |& tee -a Uji04-RT04.txt
     # Run ab
-    ab -n 400 -c 10 http://192.168.146.100:1000/ |& tee -a Uji04-Rd04.txt
+    ab -n 400 -c 10 http://192.168.146.100:1000/ |& tee -a Uji04-RT04.txt
     # Load python toggle stop
     python3 AppToggleUjiStop.py
     sleep 1
 
-    echo "selesai" |& tee -a Uji04-Rd04.txt
+    echo "selesai" |& tee -a Uji04-RT04.txt
     sleep 1    
  done
 # End loop
 
-echo $Kalimat |& tee Uji04-Rd05.txt
+echo $Kalimat |& tee Uji04-RT05.txt
 # for loop
 for i in {0..29..1}
   do 
     
     #sleep 5
-    echo "Testing number $i " |& tee -a Uji04-Rd05.txt
+    echo "Testing number $i " |& tee -a Uji04-RT05.txt
     # Load python toggle start
     python3 AppToggleUjiStart.py
-    echo "Tes request 500" |& tee -a Uji04-Rd05.txt
+    echo "Tes request 500" |& tee -a Uji04-RT05.txt
     # Run ab
-    ab -n 500 -c 10 http://192.168.146.100:1000/ |& tee -a Uji04-Rd05.txt
+    ab -n 500 -c 10 http://192.168.146.100:1000/ |& tee -a Uji04-RT05.txt
     # Load python toggle stop
     python3 AppToggleUjiStop.py
     sleep 1
 
-    echo "selesai" |& tee -a Uji04-Rd05.txt
+    echo "selesai" |& tee -a Uji04-RT05.txt
     sleep 1    
  done
 # End loop
