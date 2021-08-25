884d2e0a65d953e01261e59b064305e897bfceef
ianagung <ian.agung@len.co.id>
2021-08-26 05:30:52 +0700

    Hasil Parser

--- AppParser.sh
+++ AppParser.sh
@@ -3,181 +3,181 @@
 
 #parser Apache AB Log
 
-echo "Parser Data Uji04"
+echo "Parser Data Uji01"
 echo "Algoritma Round Robin"
-grep -w "Complete requests" Uji04-RR01.txt |& tee -a Uji04-RR-Complete-requests.txt
+grep -w "Complete requests" Uji01-RR01.txt |& tee -a Uji01-RR-Complete-requests.txt
 sleep 
-grep -w "Complete requests" Uji04-RR02.txt |& tee -a Uji04-RR-Complete-requests.txt
+grep -w "Complete requests" Uji01-RR02.txt |& tee -a Uji01-RR-Complete-requests.txt
 sleep 1
-grep -w "Complete requests" Uji04-RR03.txt |& tee -a Uji04-RR-Complete-requests.txt
+grep -w "Complete requests" Uji01-RR03.txt |& tee -a Uji01-RR-Complete-requests.txt
 sleep 1
-grep -w "Complete requests" Uji04-RR04.txt |& tee -a Uji04-RR-Complete-requests.txt
+grep -w "Complete requests" Uji01-RR04.txt |& tee -a Uji01-RR-Complete-requests.txt
 sleep 1
-grep -w "Complete requests" Uji04-RR05.txt |& tee -a Uji04-RR-Complete-requests.txt
+grep -w "Complete requests" Uji01-RR05.txt |& tee -a Uji01-RR-Complete-requests.txt
 sleep 1
 
-grep -w "Failed requests" Uji04-RR01.txt |& tee -a Uji04-RR-Failed-requests.txt
+grep -w "Failed requests" Uji01-RR01.txt |& tee -a Uji01-RR-Failed-requests.txt
 sleep 
-grep -w "Failed requests" Uji04-RR02.txt |& tee -a Uji04-RR-Failed-requests.txt
+grep -w "Failed requests" Uji01-RR02.txt |& tee -a Uji01-RR-Failed-requests.txt
 sleep 1
-grep -w "Failed requests" Uji04-RR03.txt |& tee -a Uji04-RR-Failed-requests.txt
+grep -w "Failed requests" Uji01-RR03.txt |& tee -a Uji01-RR-Failed-requests.txt
 sleep 1
-grep -w "Failed requests" Uji04-RR04.txt |& tee -a Uji04-RR-Failed-requests.txt
+grep -w "Failed requests" Uji01-RR04.txt |& tee -a Uji01-RR-Failed-requests.txt
 sleep 1
-grep -w "Failed requests" Uji04-RR05.txt |& tee -a Uji04-RR-Failed-requests.txt
+grep -w "Failed requests" Uji01-RR05.txt |& tee -a Uji01-RR-Failed-requests.txt
 sleep 1
 
-grep -w "(mean," Uji04-RR01.txt |& tee -a Uji04-RR-Timeper-requests.txt
+grep -w "(mean," Uji01-RR01.txt |& tee -a Uji01-RR-Timeper-requests.txt
 sleep 
-grep -w "(mean," Uji04-RR02.txt |& tee -a Uji04-RR-Timeper-requests.txt
+grep -w "(mean," Uji01-RR02.txt |& tee -a Uji01-RR-Timeper-requests.txt
 sleep 1
-grep -w "(mean," Uji04-RR03.txt |& tee -a Uji04-RR-Timeper-requests.txt
+grep -w "(mean," Uji01-RR03.txt |& tee -a Uji01-RR-Timeper-requests.txt
 sleep 1
-grep -w "(mean," Uji04-RR04.txt |& tee -a Uji04-RR-Timeper-requests.txt
+grep -w "(mean," Uji01-RR04.txt |& tee -a Uji01-RR-Timeper-requests.txt
 sleep 1
-grep -w "(mean," Uji04-RR05.txt |& tee -a Uji04-RR-Timeper-requests.txt
+grep -w "(mean," Uji01-RR05.txt |& tee -a Uji01-RR-Timeper-requests.txt
 sleep 1
 
-grep -w "Transfer rate:" Uji04-RR01.txt |& tee -a Uji04-RR-Transfer-rate.txt
+grep -w "Transfer rate:" Uji01-RR01.txt |& tee -a Uji01-RR-Transfer-rate.txt
 sleep 
-grep -w "Transfer rate:" Uji04-RR02.txt |& tee -a Uji04-RR-Transfer-rate.txt
+grep -w "Transfer rate:" Uji01-RR02.txt |& tee -a Uji01-RR-Transfer-rate.txt
 sleep 1
-grep -w "Transfer rate:" Uji04-RR03.txt |& tee -a Uji04-RR-Transfer-rate.txt
+grep -w "Transfer rate:" Uji01-RR03.txt |& tee -a Uji01-RR-Transfer-rate.txt
 sleep 1
-grep -w "Transfer rate:" Uji04-RR04.txt |& tee -a Uji04-RR-Transfer-rate.txt
+grep -w "Transfer rate:" Uji01-RR04.txt |& tee -a Uji01-RR-Transfer-rate.txt
 sleep 1
-grep -w "Transfer rate:" Uji04-RR05.txt |& tee -a Uji04-RR-Transfer-rate.txt
+grep -w "Transfer rate:" Uji01-RR05.txt |& tee -a Uji01-RR-Transfer-rate.txt
 sleep 1
 echo "Algoritma Random"
-grep -w "Complete requests" Uji04-Rd01.txt |& tee -a Uji04-Rd-Complete-requests.txt
+grep -w "Complete requests" Uji01-Rd01.txt |& tee -a Uji01-Rd-Complete-requests.txt
 sleep 
-grep -w "Complete requests" Uji04-Rd02.txt |& tee -a Uji04-Rd-Complete-requests.txt
+grep -w "Complete requests" Uji01-Rd02.txt |& tee -a Uji01-Rd-Complete-requests.txt
 sleep 1
-grep -w "Complete requests" Uji04-Rd03.txt |& tee -a Uji04-Rd-Complete-requests.txt
+grep -w "Complete requests" Uji01-Rd03.txt |& tee -a Uji01-Rd-Complete-requests.txt
 sleep 1
-grep -w "Complete requests" Uji04-Rd04.txt |& tee -a Uji04-Rd-Complete-requests.txt
+grep -w "Complete requests" Uji01-Rd04.txt |& tee -a Uji01-Rd-Complete-requests.txt
 sleep 1
-grep -w "Complete requests" Uji04-Rd05.txt |& tee -a Uji04-Rd-Complete-requests.txt
+grep -w "Complete requests" Uji01-Rd05.txt |& tee -a Uji01-Rd-Complete-requests.txt
 sleep 1
 
-grep -w "Failed requests" Uji04-Rd01.txt |& tee -a Uji04-Rd-Failed-requests.txt
+grep -w "Failed requests" Uji01-Rd01.txt |& tee -a Uji01-Rd-Failed-requests.txt
 sleep 
-grep -w "Failed requests" Uji04-Rd02.txt |& tee -a Uji04-Rd-Failed-requests.txt
+grep -w "Failed requests" Uji01-Rd02.txt |& tee -a Uji01-Rd-Failed-requests.txt
 sleep 1
-grep -w "Failed requests" Uji04-Rd03.txt |& tee -a Uji04-Rd-Failed-requests.txt
+grep -w "Failed requests" Uji01-Rd03.txt |& tee -a Uji01-Rd-Failed-requests.txt
 sleep 1
-grep -w "Failed requests" Uji04-Rd04.txt |& tee -a Uji04-Rd-Failed-requests.txt
+grep -w "Failed requests" Uji01-Rd04.txt |& tee -a Uji01-Rd-Failed-requests.txt
 sleep 1
-grep -w "Failed requests" Uji04-Rd05.txt |& tee -a Uji04-Rd-Failed-requests.txt
+grep -w "Failed requests" Uji01-Rd05.txt |& tee -a Uji01-Rd-Failed-requests.txt
 sleep 1
 
-grep -w "(mean," Uji04-Rd01.txt |& tee -a Uji04-Rd-Timeper-requests.txt
+grep -w "(mean," Uji01-Rd01.txt |& tee -a Uji01-Rd-Timeper-requests.txt
 sleep 
-grep -w "(mean," Uji04-Rd02.txt |& tee -a Uji04-Rd-Timeper-requests.txt
+grep -w "(mean," Uji01-Rd02.txt |& tee -a Uji01-Rd-Timeper-requests.txt
 sleep 1
-grep -w "(mean," Uji04-Rd03.txt |& tee -a Uji04-Rd-Timeper-requests.txt
+grep -w "(mean," Uji01-Rd03.txt |& tee -a Uji01-Rd-Timeper-requests.txt
 sleep 1
-grep -w "(mean," Uji04-Rd04.txt |& tee -a Uji04-Rd-Timeper-requests.txt
+grep -w "(mean," Uji01-Rd04.txt |& tee -a Uji01-Rd-Timeper-requests.txt
 sleep 1
-grep -w "(mean," Uji04-Rd05.txt |& tee -a Uji04-Rd-Timeper-requests.txt
+grep -w "(mean," Uji01-Rd05.txt |& tee -a Uji01-Rd-Timeper-requests.txt
 sleep 1
 
-grep -w "Transfer rate:" Uji04-Rd01.txt |& tee -a Uji04-Rd-Transfer-rate.txt
+grep -w "Transfer rate:" Uji01-Rd01.txt |& tee -a Uji01-Rd-Transfer-rate.txt
 sleep 
-grep -w "Transfer rate:" Uji04-Rd02.txt |& tee -a Uji04-Rd-Transfer-rate.txt
+grep -w "Transfer rate:" Uji01-Rd02.txt |& tee -a Uji01-Rd-Transfer-rate.txt
 sleep 1
-grep -w "Transfer rate:" Uji04-Rd03.txt |& tee -a Uji04-Rd-Transfer-rate.txt
+grep -w "Transfer rate:" Uji01-Rd03.txt |& tee -a Uji01-Rd-Transfer-rate.txt
 sleep 1
-grep -w "Transfer rate:" Uji04-Rd04.txt |& tee -a Uji04-Rd-Transfer-rate.txt
+grep -w "Transfer rate:" Uji01-Rd04.txt |& tee -a Uji01-Rd-Transfer-rate.txt
 sleep 1
-grep -w "Transfer rate:" Uji04-Rd05.txt |& tee -a Uji04-Rd-Transfer-rate.txt
+grep -w "Transfer rate:" Uji01-Rd05.txt |& tee -a Uji01-Rd-Transfer-rate.txt
 sleep 1
 
 echo "Algoritma Min Respon Time"
-grep -w "Complete requests" Uji04-RT01.txt |& tee -a Uji04-RT-Complete-requests.txt
+grep -w "Complete requests" Uji01-RT01.txt |& tee -a Uji01-RT-Complete-requests.txt
 sleep 
-grep -w "Complete requests" Uji04-RT02.txt |& tee -a Uji04-RT-Complete-requests.txt
+grep -w "Complete requests" Uji01-RT02.txt |& tee -a Uji01-RT-Complete-requests.txt
 sleep 1
-grep -w "Complete requests" Uji04-RT03.txt |& tee -a Uji04-RT-Complete-requests.txt
+grep -w "Complete requests" Uji01-RT03.txt |& tee -a Uji01-RT-Complete-requests.txt
 sleep 1
-grep -w "Complete requests" Uji04-RT04.txt |& tee -a Uji04-RT-Complete-requests.txt
+grep -w "Complete requests" Uji01-RT04.txt |& tee -a Uji01-RT-Complete-requests.txt
 sleep 1
-grep -w "Complete requests" Uji04-RT05.txt |& tee -a Uji04-RT-Complete-requests.txt
+grep -w "Complete requests" Uji01-RT05.txt |& tee -a Uji01-RT-Complete-requests.txt
 sleep 1
 
-grep -w "Failed requests" Uji04-RT01.txt |& tee -a Uji04-RT-Failed-requests.txt
+grep -w "Failed requests" Uji01-RT01.txt |& tee -a Uji01-RT-Failed-requests.txt
 sleep 
-grep -w "Failed requests" Uji04-RT02.txt |& tee -a Uji04-RT-Failed-requests.txt
+grep -w "Failed requests" Uji01-RT02.txt |& tee -a Uji01-RT-Failed-requests.txt
 sleep 1
-grep -w "Failed requests" Uji04-RT03.txt |& tee -a Uji04-RT-Failed-requests.txt
+grep -w "Failed requests" Uji01-RT03.txt |& tee -a Uji01-RT-Failed-requests.txt
 sleep 1
-grep -w "Failed requests" Uji04-RT04.txt |& tee -a Uji04-RT-Failed-requests.txt
+grep -w "Failed requests" Uji01-RT04.txt |& tee -a Uji01-RT-Failed-requests.txt
 sleep 1
-grep -w "Failed requests" Uji04-RT05.txt |& tee -a Uji04-RT-Failed-requests.txt
+grep -w "Failed requests" Uji01-RT05.txt |& tee -a Uji01-RT-Failed-requests.txt
 sleep 1
 
-grep -w "(mean," Uji04-RT01.txt |& tee -a Uji04-RT-Timeper-requests.txt
+grep -w "(mean," Uji01-RT01.txt |& tee -a Uji01-RT-Timeper-requests.txt
 sleep 
-grep -w "(mean," Uji04-RT02.txt |& tee -a Uji04-RT-Timeper-requests.txt
+grep -w "(mean," Uji01-RT02.txt |& tee -a Uji01-RT-Timeper-requests.txt
 sleep 1
-grep -w "(mean," Uji04-RT03.txt |& tee -a Uji04-RT-Timeper-requests.txt
+grep -w "(mean," Uji01-RT03.txt |& tee -a Uji01-RT-Timeper-requests.txt
 sleep 1
-grep -w "(mean," Uji04-RT04.txt |& tee -a Uji04-RT-Timeper-requests.txt
+grep -w "(mean," Uji01-RT04.txt |& tee -a Uji01-RT-Timeper-requests.txt
 sleep 1
-grep -w "(mean," Uji04-RT05.txt |& tee -a Uji04-RT-Timeper-requests.txt
+grep -w "(mean," Uji01-RT05.txt |& tee -a Uji01-RT-Timeper-requests.txt
 sleep 1
 
-grep -w "Transfer rate:" Uji04-RT01.txt |& tee -a Uji04-RT-Transfer-rate.txt
+grep -w "Transfer rate:" Uji01-RT01.txt |& tee -a Uji01-RT-Transfer-rate.txt
 sleep 
-grep -w "Transfer rate:" Uji04-RT02.txt |& tee -a Uji04-RT-Transfer-rate.txt
+grep -w "Transfer rate:" Uji01-RT02.txt |& tee -a Uji01-RT-Transfer-rate.txt
 sleep 1
-grep -w "Transfer rate:" Uji04-RT03.txt |& tee -a Uji04-RT-Transfer-rate.txt
+grep -w "Transfer rate:" Uji01-RT03.txt |& tee -a Uji01-RT-Transfer-rate.txt
 sleep 1
-grep -w "Transfer rate:" Uji04-RT04.txt |& tee -a Uji04-RT-Transfer-rate.txt
+grep -w "Transfer rate:" Uji01-RT04.txt |& tee -a Uji01-RT-Transfer-rate.txt
 sleep 1
-grep -w "Transfer rate:" Uji04-RT05.txt |& tee -a Uji04-RT-Transfer-rate.txt
+grep -w "Transfer rate:" Uji01-RT05.txt |& tee -a Uji01-RT-Transfer-rate.txt
 sleep 1
 echo "Algoritma Fuzy"
-grep -w "Complete requests" Uji04-FZ01.txt |& tee -a Uji04-FZ-Complete-requests.txt
+grep -w "Complete requests" Uji01-FZ01.txt |& tee -a Uji01-FZ-Complete-requests.txt
 sleep 
-grep -w "Complete requests" Uji04-FZ02.txt |& tee -a Uji04-FZ-Complete-requests.txt
+grep -w "Complete requests" Uji01-FZ02.txt |& tee -a Uji01-FZ-Complete-requests.txt
 sleep 1
-grep -w "Complete requests" Uji04-FZ03.txt |& tee -a Uji04-FZ-Complete-requests.txt
+grep -w "Complete requests" Uji01-FZ03.txt |& tee -a Uji01-FZ-Complete-requests.txt
 sleep 1
-grep -w "Complete requests" Uji04-FZ04.txt |& tee -a Uji04-FZ-Complete-requests.txt
+grep -w "Complete requests" Uji01-FZ04.txt |& tee -a Uji01-FZ-Complete-requests.txt
 sleep 1
-grep -w "Complete requests" Uji04-FZ05.txt |& tee -a Uji04-FZ-Complete-requests.txt
+grep -w "Complete requests" Uji01-FZ05.txt |& tee -a Uji01-FZ-Complete-requests.txt
 sleep 1
 
-grep -w "Failed requests" Uji04-FZ01.txt |& tee -a Uji04-FZ-Failed-requests.txt
+grep -w "Failed requests" Uji01-FZ01.txt |& tee -a Uji01-FZ-Failed-requests.txt
 sleep 
-grep -w "Failed requests" Uji04-FZ02.txt |& tee -a Uji04-FZ-Failed-requests.txt
+grep -w "Failed requests" Uji01-FZ02.txt |& tee -a Uji01-FZ-Failed-requests.txt
 sleep 1
-grep -w "Failed requests" Uji04-FZ03.txt |& tee -a Uji04-FZ-Failed-requests.txt
+grep -w "Failed requests" Uji01-FZ03.txt |& tee -a Uji01-FZ-Failed-requests.txt
 sleep 1
-grep -w "Failed requests" Uji04-FZ04.txt |& tee -a Uji04-FZ-Failed-requests.txt
+grep -w "Failed requests" Uji01-FZ04.txt |& tee -a Uji01-FZ-Failed-requests.txt
 sleep 1
-grep -w "Failed requests" Uji04-FZ05.txt |& tee -a Uji04-FZ-Failed-requests.txt
+grep -w "Failed requests" Uji01-FZ05.txt |& tee -a Uji01-FZ-Failed-requests.txt
 sleep 1
 
-grep -w "(mean," Uji04-FZ01.txt |& tee -a Uji04-FZ-Timeper-requests.txt
+grep -w "(mean," Uji01-FZ01.txt |& tee -a Uji01-FZ-Timeper-requests.txt
 sleep 
-grep -w "(mean," Uji04-FZ02.txt |& tee -a Uji04-FZ-Timeper-requests.txt
+grep -w "(mean," Uji01-FZ02.txt |& tee -a Uji01-FZ-Timeper-requests.txt
 sleep 1
-grep -w "(mean," Uji04-FZ03.txt |& tee -a Uji04-FZ-Timeper-requests.txt
+grep -w "(mean," Uji01-FZ03.txt |& tee -a Uji01-FZ-Timeper-requests.txt
 sleep 1
-grep -w "(mean," Uji04-FZ04.txt |& tee -a Uji04-FZ-Timeper-requests.txt
+grep -w "(mean," Uji01-FZ04.txt |& tee -a Uji01-FZ-Timeper-requests.txt
 sleep 1
-grep -w "(mean," Uji04-FZ05.txt |& tee -a Uji04-FZ-Timeper-requests.txt
+grep -w "(mean," Uji01-FZ05.txt |& tee -a Uji01-FZ-Timeper-requests.txt
 sleep 1
 
-grep -w "Transfer rate:" Uji04-FZ01.txt |& tee -a Uji04-FZ-Transfer-rate.txt
+grep -w "Transfer rate:" Uji01-FZ01.txt |& tee -a Uji01-FZ-Transfer-rate.txt
 sleep 
-grep -w "Transfer rate:" Uji04-FZ02.txt |& tee -a Uji04-FZ-Transfer-rate.txt
+grep -w "Transfer rate:" Uji01-FZ02.txt |& tee -a Uji01-FZ-Transfer-rate.txt
 sleep 1
-grep -w "Transfer rate:" Uji04-FZ03.txt |& tee -a Uji04-FZ-Transfer-rate.txt
+grep -w "Transfer rate:" Uji01-FZ03.txt |& tee -a Uji01-FZ-Transfer-rate.txt
 sleep 1
-grep -w "Transfer rate:" Uji04-FZ04.txt |& tee -a Uji04-FZ-Transfer-rate.txt
+grep -w "Transfer rate:" Uji01-FZ04.txt |& tee -a Uji01-FZ-Transfer-rate.txt
 sleep 1
-grep -w "Transfer rate:" Uji04-FZ05.txt |& tee -a Uji04-FZ-Transfer-rate.txt
+grep -w "Transfer rate:" Uji01-FZ05.txt |& tee -a Uji01-FZ-Transfer-rate.txt
 sleep 1
