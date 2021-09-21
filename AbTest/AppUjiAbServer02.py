c45f94ed9d8d2fcc3248001ee18a244956971f3a
ianagung <ian.agung@len.co.id>
2021-09-21 16:28:46 +0700

    http server mqtt

--- AbTest/AppUjiAbServer02.py
+++ AbTest/AppUjiAbServer02.py
@@ -5,6 +5,32 @@ from os import curdir,sep
 #Create a index.html aside the code
 #Run: python server.py
 #After run, try http://localhost:8080/
+broker_url = "192.168.53.101"
+broker_port = 1883
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected With Result Code " ,rc)
+    if rc==0:
+        print("connected OK Returned code=",rc)
+    else:
+        print("Bad connection Returned code=",rc)
+
+def on_disconnect(client, userdata, rc):
+    print("Client Got Disconnected")
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_disconnect = on_disconnect
+#To Process Every Other Message
+#client.on_message = on_message
+# edit code for passwords
+print("setting  password")
+client.username_pw_set(username="user01",password="mqtt")
+
+client.connect(broker_url, broker_port)
+
+#msg = "1"
+print("Pengujian Load Balancing dimulai")
 
 class RequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -22,6 +48,10 @@ class RequestHandler(BaseHTTPRequestHandler):
                 self.end_headers()
                 self.wfile.write(f.read())
                 f.close()
+                msg = "1"
+                client.publish(topic="sdn/request01", payload=msg, qos=0, retain=False)
+                # client.publish(topic="sdn/request02", payload=msg, qos=0, retain=False)
+                # client.publish(topic="sdn/request02", payload=msg, qos=0, retain=False)
             return
         except IOError:
             self.send_error(404,'File not found!')
