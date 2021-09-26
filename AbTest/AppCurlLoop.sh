e42a567db1682e26ef1516af481a5880d1391dde
ianagung <ian.agung@len.co.id>
2021-09-26 15:51:23 +0700

    curl loop

--- AbTest/AppCurlLoop.sh
+++ AbTest/AppCurlLoop.sh
@@ -6,7 +6,7 @@ echo "infinite loops [ hit CTRL+C to stop]"
 # for loop
 for i in {0..36000..1}
   do 
-    curl http://192.168.146.100:8080/index.html
+    curl --silent http://192.168.146.100:8080/index.html > /dev/null
     sleep 1    
  done
 # End loop
