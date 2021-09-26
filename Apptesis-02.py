1dfc66246e1b2ff0b295b2ec4d182e72cf629a4c
ianagung <ian.agung@len.co.id>
2021-09-27 05:23:07 +0700

    Uji05 Fuzzy

--- Apptesis-02.py
+++ Apptesis-02.py
@@ -93,8 +93,8 @@ mean_cpu_server03 = 1
 mean_mem_server01 = 1
 mean_mem_server02 = 1
 mean_mem_server03 = 1
-f_name = 'Uji05-RT01.csv'
-f_name2 = 'Uji05-RT02.csv' #Value of CPU Memory Network per sec for time series analysis
+f_name = 'Uji05-FZ01.csv'
+f_name2 = 'Uji05-FZ02.csv' #Value of CPU Memory Network per sec for time series analysis
 # getting length of list
 lengths = len(listserver)
 serverCount = 1
@@ -342,7 +342,7 @@ def job1Fuzzy():
     # Iterating the index
     # same as 'for i in range(len(list))'
     for i in range(lengths):
-        print("Perhitungan Fuzzy - Perubahan Load Window server-", listserver[i])
+        #print("Perhitungan Fuzzy - Perubahan Load Window server-", listserver[i])
 
         if listserver[i]==1:
             cpu_val = cpu01
@@ -437,7 +437,7 @@ def job1MinRT():
 	#client.publish(topic="sdn/cpumem01", payload=msg, qos=1, retain=False)
 	#client.publish(topic="sdn/rsptm03", payload=rsp_tm_s3, qos=0, retain=False)
 # This timer will run job() five times, one second apart
-timer1 = multitimer.MultiTimer(interval=2, function=job1MinRT, count=-1)
+timer1 = multitimer.MultiTimer(interval=2, function=job1Fuzzy, count=-1)
 # Also, this timer would run indefinitely...
 timer1.start()
 
@@ -470,7 +470,7 @@ def job2():
 # This timer will run job() five times, one second apart
 timer2 = multitimer.MultiTimer(interval=1, function=job2, count=-1)
 # Also, this timer would run indefinitely...
-timer2.start()
+#timer2.start()
 
 #fungsi tes
 # def job3():
